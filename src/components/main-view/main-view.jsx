b217bc36a2333f1c3f6c12889a7b13f0a4b24873
Tanimara Elias Santos <36615712+anthropovixen@users.noreply.github.com>
2021-03-19 20:44:35 +1100

    update component logic for MainView to GET movies

--- src/components/main-view/main-view.jsx
+++ src/components/main-view/main-view.jsx
@@ -1,14 +1,31 @@
-import React from 'react';
 import axios from 'axios';
 
-class MainView extends React.Component {
-	constructor() {
-		super();
-
-		this.state = {};
+export class MainView extends React.Component {
+	componentDidMount() {
+		axios
+			.get('<https://myflix-movies-app.herokuapp.com/movies')
+			.then((response) => {
+				this.setState({
+					Movies: response.data,
+				});
+			})
+			.catch(function (error) {
+				console.log(error);
+			});
 	}
 
 	render() {
-		return <div className="main-view"></div>;
+		const { Movies } = this.state;
+		if (!Movies) return <div className="main-view" />;
+
+		return (
+			<div className="main-view">
+				{Movies.map((Movie) => (
+					<div className="movie-card" key={Movie._id}>
+						{Movie.Title}
+					</div>
+				))}
+			</div>
+		);
 	}
 }
