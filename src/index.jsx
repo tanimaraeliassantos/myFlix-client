fc2816c4bf306bf0bf9560c99febfc1bb79be8f7
Tanimara Elias Santos <36615712+anthropovixen@users.noreply.github.com>
2021-03-19 22:45:14 +1100

    Update index.jsx to import MainView

--- src/index.jsx
+++ src/index.jsx
@@ -1,17 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+import { MainView } from './components/main-view/main-view.jsx';
+
 // Import statement to indicate that you need to bundle `./index.scss`
 import './index.scss';
 
 // Main component (will eventually use all the others)
 class MyFlixApplication extends React.Component {
 	render() {
-		return (
-			<div className="my-flix">
-				<div>Good morning</div>
-			</div>
-		);
+		return <MainView />;
 	}
 }
 
