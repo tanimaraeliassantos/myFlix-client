c280f01304ef45c47d8ce85bce3b14ee78aefc86
Tanimara Elias Santos <36615712+anthropovixen@users.noreply.github.com>
2021-03-19 14:42:50 +1100

    Declare component and create empty state object

--- src/components/main-view/main-view.jsx
+++ src/components/main-view/main-view.jsx
@@ -1,2 +1,14 @@
 import React from 'react';
 import axios from 'axios';
+
+class MainView extends React.Component {
+	constructor() {
+		super();
+
+		this.state = {};
+	}
+
+	render() {
+		return <div className="main-view"></div>;
+	}
+}
