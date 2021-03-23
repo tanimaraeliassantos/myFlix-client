1a859de857b914998f2401c176a692849d8324e5
Tanimara Elias Santos <36615712+anthropovixen@users.noreply.github.com>
2021-03-24 09:56:41 +1100

    Update to main-view.jsx

--- src/components/main-view/main-view.jsx
+++ src/components/main-view/main-view.jsx
@@ -5,7 +5,7 @@ export class MainView extends React.Component {
 	// One of the "hooks" available in a React Component
 	componentDidMount() {
 		axios
-			.get('<my-api-endpoint/movies>')
+			.get('https://myflix-movies-app.herokuapp.com/movies')
 			.then((response) => {
 				// Assign the result to the state
 				this.setState({
