5cec47486c790ae9ad466379dc8ce3de19af389d
Tanimara Elias Santos <36615712+anthropovixen@users.noreply.github.com>
2021-03-24 10:59:19 +1100

    Display MovieView and add click event

--- src/components/main-view/main-view.jsx
+++ src/components/main-view/main-view.jsx
@@ -1,8 +1,19 @@
 import React from 'react';
 import axios from 'axios';
 
+import { MovieCard } from '../movie-card/movie-card';
+import { MovieView } from '../movie-view/movie-view';
+
 export class MainView extends React.Component {
-	// One of the "hooks" available in a React Component
+	constructor() {
+		super();
+
+		this.state = {
+			movies: null,
+			selectedMovie: null,
+		};
+	}
+
 	componentDidMount() {
 		axios
 			.get('https://myflix-movies-app.herokuapp.com/movies')
@@ -17,9 +28,13 @@ export class MainView extends React.Component {
 			});
 	}
 
+	onMovieClick(movie) {
+		this.setState({
+			selectedMovie: movie,
+		});
+	}
+
 	render() {
-		// If the state isn't initialized, this will throw on runtime
-		// before the data is initially loaded
 		const { movies } = this.state;
 
 		// Before the movies have been loaded
@@ -27,11 +42,17 @@ export class MainView extends React.Component {
 
 		return (
 			<div className="main-view">
-				{movies.map((movie) => (
-					<div className="movie-card" key={movie._id}>
-						{movie.Title}
-					</div>
-				))}
+				{selectedMovie ? (
+					<MovieView movie={selectedMovie} />
+				) : (
+					movies.map((movie) => (
+						<MovieCard
+							key={movie._id}
+							movie={movie}
+							onClick={(movie) => this.onMovieClick(movie)}
+						/>
+					))
+				)}
 			</div>
 		);
 	}
