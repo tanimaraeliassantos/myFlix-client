63b4b39e4295cfda5229dee9539d2d673a9b931c
Tanimara Elias Santos <36615712+anthropovixen@users.noreply.github.com>
2021-03-24 10:58:12 +1100

    Add onClick to MovieCard component

--- src/components/movie-card/movie-card.jsx
+++ src/components/movie-card/movie-card.jsx
@@ -2,8 +2,12 @@ import React from 'react';
 
 export class MovieCard extends React.Component {
 	render() {
-		const { movie } = this.props;
+		const { movie, onClick } = this.props;
 
-		return <div className="movie-card">{movie.Title}</div>;
+		return (
+			<div onClick={() => onClick(movie)} className="movie-card">
+				{movie.Title}
+			</div>
+		);
 	}
 }
