970376a5ae83f73faa458f8eaf2a2fe5319d98e9
Tanimara Elias Santos <36615712+anthropovixen@users.noreply.github.com>
2021-03-24 09:46:08 +1100

    Update to main-view.jsx

--- src/components/main-view/main-view.jsx
+++ src/components/main-view/main-view.jsx
@@ -1,25 +1,15 @@
 import React from 'react';
 import axios from 'axios';
 
-import { MovieCard } from '../movie-card/movie-card';
-import { MovieView } from '../movie-view/movie-view';
-
 export class MainView extends React.Component {
-	constructor() {
-		super();
-
-		this.state = {
-			movies: null,
-			selectedMovie: null,
-		};
-	}
-
+	// One of the "hooks" available in a React Component
 	componentDidMount() {
 		axios
-			.get('https://myflix-movies-app.herokuapp.com/movies')
+			.get('<my-api-endpoint/movies>')
 			.then((response) => {
+				// Assign the result to the state
 				this.setState({
-					Movies: response.data,
+					movies: response.data,
 				});
 			})
 			.catch(function (error) {
@@ -27,31 +17,21 @@ export class MainView extends React.Component {
 			});
 	}
 
-	onMovieClick(movie) {
-		this.setState({
-			selectedMovie: movie,
-		});
-	}
-
 	render() {
-		const { movies, selectedMovie } = this.state;
+		// If the state isn't initialized, this will throw on runtime
+		// before the data is initially loaded
+		const { movies } = this.state;
 
 		// Before the movies have been loaded
 		if (!movies) return <div className="main-view" />;
 
 		return (
 			<div className="main-view">
-				{selectedMovie ? (
-					<MovieView movie={selectedMovie} />
-				) : (
-					movies.map((movie) => (
-						<MovieCard
-							key={movie._id}
-							movie={movie}
-							onClick={(movie) => this.onMovieClick(movie)}
-						/>
-					))
-				)}
+				{movies.map((movie) => (
+					<div className="movie-card" key={movie._id}>
+						{movie.Title}
+					</div>
+				))}
 			</div>
 		);
 	}
