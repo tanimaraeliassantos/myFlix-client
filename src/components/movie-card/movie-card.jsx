086a212a96f8dee64892c35c9cd1d049e7a29918
Tanimara Elias Santos <36615712+anthropovixen@users.noreply.github.com>
2021-03-26 09:51:59 +1100

    Update movie-cardjsx to add Back button functionality

--- src/components/movie-card/movie-card.jsx
+++ src/components/movie-card/movie-card.jsx
@@ -2,10 +2,10 @@ import React from 'react';
 
 export class MovieCard extends React.Component {
 	render() {
-		const { movie, onClick } = this.props;
+		const { movie, onMovieClick } = this.props;
 
 		return (
-			<div onClick={() => onClick(movie)} className="movie-card">
+			<div onClick={() => onMovieClick(movie)} className="movie-card">
 				{movie.Title}
 			</div>
 		);
