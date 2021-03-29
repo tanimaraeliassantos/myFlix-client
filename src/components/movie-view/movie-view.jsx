d9baa848a3b7a4866046ef59effb07237e9cf373
Tanimara Elias Santos <36615712+anthropovixen@users.noreply.github.com>
2021-03-29 16:32:52 +1100

    Remove event listener with componentWillUnmount

--- src/components/movie-view/movie-view.jsx
+++ src/components/movie-view/movie-view.jsx
@@ -9,6 +9,10 @@ export class MovieView extends React.Component {
 		document.addEventListener('keypress', this.keypressCallback);
 	}
 
+	componentWillUnmount() {
+		document.removeEventListener('keypress', this.keypressCallback);
+	}
+
 	render() {
 		const { movie, onBackClick } = this.props;
 
