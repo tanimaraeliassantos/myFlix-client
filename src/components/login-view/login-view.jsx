521ba1db5d30375d9d047285feda33c44975aedd
Tanimara Elias Santos <36615712+anthropovixen@users.noreply.github.com>
2021-04-16 09:17:57 +1000

    Update login-view way of importing

--- src/components/login-view/login-view.jsx
+++ src/components/login-view/login-view.jsx
@@ -1,7 +1,6 @@
 import React, { useState } from 'react';
-import Form from 'react-bootstrap/Form';
-import Button from 'react-bootstrap/Button';
 import axios from 'axios';
+import { Form, Button } from 'react-bootstrap';
 
 export function LoginView(props) {
 	const [username, setUsername] = useState('');
